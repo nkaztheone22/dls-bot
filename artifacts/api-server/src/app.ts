25b7de50300dcd7e52034e722306ab18591ea972
nkaztheone22 <nkazanasibanda1983@gmail.com>
2026-05-06 04:01:48 +0200

    add app.ts

---
+++ artifacts/api-server/src/app.ts
@@ -0,0 +1,39 @@
+import express, { type Express } from "express";
+import cors from "cors";
+import pinoHttp from "pino-http";
+import router from "./routes/index.js";
+import { logger } from "./lib/logger.js";
+import { startBot } from "./bot/bot.js";
+
+const app: Express = express();
+
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req) {
+        return {
+          id: req.id,
+          method: req.method,
+          url: req.url?.split("?")[0],
+        };
+      },
+      res(res) {
+        return {
+          statusCode: res.statusCode,
+        };
+      },
+    },
+  }),
+);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+startBot().catch((err) => {
+  logger.error({ err }, "Failed to start bot on init");
+});
+
+export default app;
